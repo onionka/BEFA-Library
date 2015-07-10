203153c6c6641428fd5e725a43f39fe9c4be7429
Miroslav Cibulka <xcibul10@stud.fit.vutbr.cz>
2015-07-10 23:58:42 +0200

    coveralls mistake

--- travis/post_script.sh
+++ travis/post_script.sh
@@ -10,8 +10,9 @@ fi
 echo -en "travis_fold:end:\e[0;33m Makeing coverage \e[0m\r"
 
 echo -en "travis_fold:start:\e[0;33m Uploading coverage \e[0m\r"
-coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+coveralls-lcov --repo-token=${COVERALLS_TOKEN} coverage.info
 return_code=$?
+echo "coveralls-lcov --repo-token=${COVERALLS_TOKEN} coverage.info"
 if [ ${return_code} -ne 0 ]; then
     exit ${return_code}
 fi
